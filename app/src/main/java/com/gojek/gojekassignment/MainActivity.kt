8e6ff1f5cd85b80b97c17761ce920b26390da86f
laatonwalabhoot <aishwarya.mailin@gmail.com>
2021-06-11 08:43:06 +0530

    initial commit

---
+++ app/src/main/java/com/gojek/gojekassignment/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.gojek.gojekassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gojek.gojekassignment.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/gojek/gojekassignment/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.gojek.gojekassignment.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
